c40e5cb733d9d6405d65cb039d1baae0d75eaf0d
Djdaan Truong <daantd.a12017@gmail.com>
2022-06-12 17:19:07 +0700

    update practice

--- easyfrontend/practice/string-02.js
+++ easyfrontend/practice/string-02.js
@@ -1,18 +1,18 @@
-export function statisticsWords(str) {
-  if (str === '') return {}
-  const statistics = {}
-  str
-    .split(' ')
-    .filter((x) => x !== '')
-    .forEach((x) => {
-      if (statistics[x]) {
-        statistics[x] += 1
-      } else {
-        statistics[x] = 1
-      }
-    })
-  return statistics
-}
+// export function statisticsWords(str) {
+//   if (str === '') return {}
+//   const statistics = {}
+//   str
+//     .split(' ')
+//     .filter((x) => x !== '')
+//     .forEach((x) => {
+//       if (statistics[x]) {
+//         statistics[x] += 1
+//       } else {
+//         statistics[x] = 1
+//       }
+//     })
+//   return statistics
+// }
 
 export function statisticsWords(str) {
   if (str === '') return {}
